3b368a6244f074967f43a1694f014d1b984f8a3e
Athiwat Sirinantipak <asirinantipak@gmail.com>
2023-11-29 00:21:48 +0700

    Create 11.Mathematics Calculation.R

---
+++ 11.Mathematics Calculation.R
@@ -0,0 +1,52 @@
+##Basic R
+
+##R can be used for general arithmetic calculation
+
+#Type in the number 
+5
+
+#Summation
+5+5
+
+#Subtraction
+5-5
+
+#Multiplication
+5*5
+
+#Division
+5/5
+
+#Squared, to the power of...
+5^2
+
+5^3
+
+5**2
+
+5**3
+
+#Square root
+sqrt(25)
+sqrt(5^2)
+sqrt(5**2)
+
+#Logarithm function
+log10(10)
+log10(10^2)
+log10(100)
+log10(10^1)
+
+
+#Natural logarithm function
+exp(1)
+#2.178282
+log(10)
+2.718282^2.302585
+log(exp(2.302585))
+
+
+#More complex calculations
+(3+5)*(5+3)
+sqrt((3+5)*(5+3))
+(3+5)/2*(5+3)/4
